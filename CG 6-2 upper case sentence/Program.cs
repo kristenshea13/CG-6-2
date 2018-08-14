e1a5b7ce01ff3c79510e22b2a8cd15db8a71f13e
Kristen Shea <kstar61@live.com>
2018-08-13 21:09:18 -0500

    Add project files.

---
+++ CG 6-2 upper case sentence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_6_2_upper_case_sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
